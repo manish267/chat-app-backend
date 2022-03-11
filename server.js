0e4884f07ed043350c26219e56d77e3eaf6cdca5
manish267 <ymanish26798@gmail.com>
2022-03-11 19:17:18 +0530

    chat app

---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const { chats } = require("./data/data.js");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+app.use(cors());
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json("API is Running");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+// app.get("/api/chat", (req, res) => {
+//   res.send(chats);
+// });
+
+// app.get("/api/chat/:id", (req, res) => {
+//   console.log(req.params.id);
+
+//   const singleChat = chats.find((c) => c._id === req.params.id);
+
+//   res.status(200).json(singleChat);
+// });
+
+app.use(notFound);
+app.use(errorHandler);
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`server is running at port ${process.env.PORT}`)
+);
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+    console.log(userData._id);
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
+
+  socket.on("stop typing", (room) => {
+    socket.in(room).emit("stop typing");
+  });
+
+  socket.on("new message", (newMessageReceived) => {
+    var chat = newMessageReceived.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id === newMessageReceived.sender._id) return;
+
+      socket.in(user._id).emit("message received", newMessageReceived);
+    });
+  });
+
+  socket.off("setup", () => {
+    socket.leave(userData._id);
+  });
+});
